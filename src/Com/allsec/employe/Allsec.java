720194cdecc957c6edd33137fcaa049324f4845f
JacobSaxena <jacobsaxena10@gmail.com>
2021-08-30 14:19:51 +0530

    master

---
+++ src/Com/allsec/employe/Allsec.java
@@ -0,0 +1,23 @@
+package Com.allsec.employe;
+
+public class Allsec {
+	public void employeeid()
+	{
+		System.out.println("employee id is 7000850");
+		
+	}
+	public void processname()
+	{
+		System.out.println("PROCESS NAME IS SONIC");
+	}
+	public void shifttime()
+	{
+		System.out.println("time is 9.00 to 18.00");
+	}
+public static void main(String[] args) {
+	Allsec tech=new Allsec();
+	tech.employeeid();
+	tech.processname();
+	tech.shifttime();
+}
+}
